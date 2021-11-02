4b80904c6a9fa11e85428f3267c5be5b076d1d26
Andrew Arnott <andrewarnott@live.com>
2021-11-02 10:47:24 -0600

    Capture the trx files in testResults artifact

--- azure-pipelines/artifacts/testResults.ps1
+++ azure-pipelines/artifacts/testResults.ps1
@@ -1,8 +1,11 @@
 if ($env:AGENT_TEMPDIRECTORY) {
     # The DotNetCoreCLI uses an alternate location to publish these files
     $guidRegex = '^[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12}$'
+    $files = @()
+    $files += Get-ChildItem $env:AGENT_TEMPDIRECTORY -Directory |? { $_.Name -match $guidRegex } |% { Get-ChildItem "$($_.FullName)\dotnet*.dmp","$($_.FullName)\testhost*.dmp","$($_.FullName)\Sequence_*.xml" -Recurse }
+    $files += Get-ChildItem $env:AGENT_TEMPDIRECTORY\*.trx
     @{
-        $env:AGENT_TEMPDIRECTORY = (Get-ChildItem $env:AGENT_TEMPDIRECTORY -Directory |? { $_.Name -match $guidRegex } |% { Get-ChildItem "$($_.FullName)\dotnet*.dmp","$($_.FullName)\testhost*.dmp","$($_.FullName)\Sequence_*.xml" -Recurse });
+        $env:AGENT_TEMPDIRECTORY = $files;
     }
 } else {
     $testRoot = Resolve-Path "$PSScriptRoot\..\..\test"
