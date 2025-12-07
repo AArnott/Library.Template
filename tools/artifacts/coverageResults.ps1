eaf140e439365c3faadcf1664fe27a1262b0a5a1
Andrew Arnott <andrewarnott@live.com>
2025-12-07 12:05:03 -0700

    Fix code coverage collection w/ MTP

--- tools/artifacts/coverageResults.ps1
+++ tools/artifacts/coverageResults.ps1
@@ -1,18 +1,12 @@
-$RepoRoot = [System.IO.Path]::GetFullPath("$PSScriptRoot\..\..")
+$RepoRoot = Resolve-Path "$PSScriptRoot\..\.."
 
-$coverageFiles = @(Get-ChildItem "$RepoRoot/test/*.cobertura.xml" -Recurse | Where {$_.FullName -notlike "*/In/*"  -and $_.FullName -notlike "*\In\*" })
+$coverageFiles = @(Get-ChildItem "$RepoRoot/*.cobertura.xml" -Recurse | Where-Object {$_.FullName -notlike "*/In/*"  -and $_.FullName -notlike "*\In\*" })
 
 # Prepare code coverage reports for merging on another machine
-$repoRoot = $env:SYSTEM_DEFAULTWORKINGDIRECTORY
-if (!$repoRoot) { $repoRoot = $env:GITHUB_WORKSPACE }
-if ($repoRoot) {
-    Write-Host "Substituting $repoRoot with `"{reporoot}`""
-    $coverageFiles |% {
-        $content = Get-Content -LiteralPath $_ |% { $_ -Replace [regex]::Escape($repoRoot), "{reporoot}" }
-        Set-Content -LiteralPath $_ -Value $content -Encoding UTF8
-    }
-} else {
-    Write-Warning "coverageResults: Cloud build not detected. Machine-neutral token replacement skipped."
+Write-Host "Substituting $repoRoot with `"{reporoot}`""
+$coverageFiles |% {
+    $content = Get-Content -LiteralPath $_ |% { $_ -Replace [regex]::Escape($repoRoot), "{reporoot}" }
+    Set-Content -LiteralPath $_ -Value $content -Encoding UTF8
 }
 
 if (!((Test-Path $RepoRoot\bin) -and (Test-Path $RepoRoot\obj))) { return }
